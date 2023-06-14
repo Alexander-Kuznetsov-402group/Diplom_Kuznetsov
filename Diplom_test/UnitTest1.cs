adc24242121b79e6b3c0729e45409419c4738d34
Алексей Писарев <afonasiu2983@gmail.com>
2023-06-14 11:02:57 +1000

    готово

---
+++ Diplom_test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class avtorixahiyaTests
+    {
+        [TestMethod()]
+        public void avtorixahiyaTest1()
+        {
+            string tb2 = "";
+            string tb1 = "";
+            Assert.AreEqual(tb2, tb1);
+        }
+    }
+}
